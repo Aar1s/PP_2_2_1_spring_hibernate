3665f0de541f5777d84379beb0c0b1178f0ce73e
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-05-18 20:54:13 +0500

    First Try/DI Constructor

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -15,10 +15,9 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
-      Car car1 = new Car("Model 1", 1);
-      Car car2 = new Car("Model 2", 2);
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Model 1", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Model 2", 2)));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
       List<User> users = userService.listUsers();
